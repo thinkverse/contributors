0c4a61c2f41b8c9c2a83589ed4a5c79d6df62ebb
Kim Hallberg <hallberg.kim@gmail.com>
2020-08-05 12:36:39 +0200

    Getting all avatars (#9)
    * Add parse-link-header dependency
    * Update timeout time for larger renders
    * Add centra for api requests
    * Update example env with new keys
    * Add new avatars api
    * Update renderer to use new api

--- public/app.js
+++ public/app.js
@@ -1,11 +1,6 @@
 const container = document.querySelector('#contributors')
 const paramaters = new URLSearchParams(window.location.search)
 
-const repository = paramaters.get('repo') ?? 'git-badges'
-const organization = paramaters.get('org') ?? 'thinkverse'
-
-const headers = { 'User-Agent': organization }
-
 function renderAvatarImage (source) {
   const imageTag = document.createElement('img')
   imageTag.src = source
@@ -13,18 +8,14 @@ function renderAvatarImage (source) {
   container.appendChild(imageTag)
 }
 
-async function getContributors (org, repo) {
-  const response = await fetch(`https://api.github.com/repos/${org}/${repo}/contributors?anon=1`, headers)
+async function getContributors () {
+  const response = await fetch(`api/avatars/?${paramaters.toString()}`)
 
   return await response.json()
 }
 
-getContributors(organization, repository).then(contributors => {
-  contributors.filter(contributor =>
-    ('login' in contributor && 'avatar_url' in contributor) &&
-        !contributor.login.includes('[bot]'))
-    .map(contributor => contributor.avatar_url)
-    .forEach(avatar => renderAvatarImage(avatar))
+getContributors().then(avatar => {
+  avatar.forEach(avatar => renderAvatarImage(avatar))
 
   container.classList.add('rendered')
 })
